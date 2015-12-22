7edbd29cd1e52cc7af70ec1328c6072ec5a31805
Ipsita Mandal <ipsita@linux-gfuc.site>
2015-12-22 16:38:07 -0500

    working code

--- rho-linearV.cc
+++ rho-linearV.cc
@@ -28,14 +28,14 @@ int main (int argc, const char * argv[])
     const double delta = 0.1 , t=1, V=0.01, mu=0 ;
 
 
-    double N  = 10;//lattice size 
+    double N  = 20;//lattice size 
 
-    for ( double rx = rrx; rx <= N; rx += 1)
+    for ( double rx = rrx; rx < N+ THRES; rx += 1)
     {
         rxvec.push_back( double(rx) );
     }
 
-    for ( double ry = rry; ry <= N; ry += 1)
+    for ( double ry = rry; ry < N+THRES; ry += 1)
     {
         ryvec.push_back( double(ry) );
     }
@@ -44,19 +44,19 @@ int main (int argc, const char * argv[])
     {
         for ( double rry : ryvec)
         {   cx_double rhosum(0);
-            for (double k1y = kky; k1y <=2*Pi; k1y += 2*Pi/N)
+            for (double k1y = kky; k1y <2*Pi+THRES; k1y += 2*Pi/N)
              {  for (double k1x = kkx; k1x <=2*Pi; k1x += 2*Pi/N)
 		 { double e1(0);
 		   e1 = -2*t* ( cos(k1x)+ cos(k1y) ) -mu;
 		 
-		   { for(double k2y = kky; k2y <=2*Pi; k2y += 2*Pi/N)
-                     { for(double k2x = kkx; k2x <=2*Pi; k2x += 2*Pi/N)
+		   { for(double k2y = kky; k2y <=2*Pi+THRES; k2y += 2*Pi/N)
+                     { for(double k2x = kkx; k2x <=2*Pi+THRES ; k2x += 2*Pi/N)
                          { double e2(0);
 		           e2 = -2*t* ( cos(k2y)+ cos(k2x) ) -mu;
 		           rhosum += 2*Pi*V*(-1+ e1*e2/sqrt((e1*e1+delta*delta)*(e2*e2    +delta*delta)))
                                      *exp(1.0*(k2x - k1x)*rrx*ii)*exp(1.0*(k2y - 
                                                                          k1y)*rry*ii)
-                                     / ( sqrt(e1*delta+delta*delta) + sqrt(e2*e2+delta*delta) );
+                                     / ( sqrt(e1*e1+delta*delta) + sqrt(e2*e2+delta*delta) );
             
                           }
                       }
