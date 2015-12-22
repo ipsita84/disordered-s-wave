d7f76d4328f3bf0a30954f3b124cdafcdc5daa4f
Ipsita Mandal <ipsita@linux-gfuc.site>
2015-12-22 13:26:25 -0500

    f

--- rho-linearV.cc
+++ rho-linearV.cc
@@ -13,9 +13,10 @@ using namespace arma;
 int main (int argc, const char * argv[])
 {
     vector <double> rxvec, ryvec, rho;
+    const cx_double ii(0,1);
     const double Pi = math::pi();
-    double rrx=0, rry=0;
-    const double delta = 0.1 , t=1, V=0.01 ;
+    double rrx=0, rry=0, kky=0, kkx=0;
+    const double delta = 0.1 , t=1, V=0.01, mu=0 ;
 
 
     unsigned int N  = 60;//lattice size 
@@ -34,19 +35,19 @@ int main (int argc, const char * argv[])
     {
         for (double rry : ryvec)
         {   double rhosum(0);
-            for (double k1y = kky; ky <=2*Pi; k1y += 2*Pi/N)
-             {  for (double k1x = kkx; kx <=2*Pi; ky += 2*Pi/N)
+            for (double k1y = kky; k1y <=2*Pi; k1y += 2*Pi/N)
+             {  for (double k1x = kkx; k1x <=2*Pi; k1x += 2*Pi/N)
 		 double e1(0);
 		 e1 = -2*t* ( cos(k1x)+ cos(k1y) ) -mu;
 		 
-		  { for(double k2y = kky; ky <=2*Pi; k2y += 2*Pi/N)
-                     { for(double k2x = kkx; ky <=2*Pi; k2x += 2*Pi/N)
+		  { for(double k2y = kky; k2y <=2*Pi; k2y += 2*Pi/N)
+                     { for(double k2x = kkx; k2x <=2*Pi; k2x += 2*Pi/N)
                          { double e2(0);
 		           e2 = -2*t* ( cos(k2y)+ cos(k2x) ) -mu;
-		           rhosum += 2*Pi*V*(-1+ e1*e2/sqrt((e1^2+delta^2)*(e2^2+delta^2)))
-                                     * exp(1.0*(k2x - k1x)*rrx*ii)** exp(1.0*(k2y - 
+		           rhosum += 2*Pi*V*(-1+ e1*e2/sqrt((e1*e1+delta*delta)*(e2*e2+delta*delta)))
+                                     *exp(1.0*(k2x - k1x)*rrx*ii)*exp(1.0*(k2y - 
                                                                          k1y)*rry*ii)
-                                     / ( sqrt(e1^2+delta^2) + sqrt(e2^2+delta^2) );
+                                     / ( sqrt(e1*delta+delta*delta) + sqrt(e2*e2+delta*delta) );
             
                           }
                       }
